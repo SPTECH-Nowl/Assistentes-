6937230200e05ec8fa2e9eef759c7ec1648564c2
dantaswilljpgdev <dantaswill748@gmail.com>
2023-10-21 00:44:52 -0300

    nova version

--- install_java.sh
+++ install_java.sh
@@ -3,88 +3,72 @@
 PURPLE='\033[0;35m'
 NC='\033[0m'
 VERSAO=17
-echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Olá usuário, serei seu assistente para instalação do Java!"
-echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Verificando se você possui o Java instalado na sua máquina!"
-sleep 7
-# Verifica se o Java está instalado
+
+# Função para exibir mensagem com pausa
+show_message() {
+    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} $1"
+    read -p "Pressione Enter para continuar..."
+}
+
+show_message "Olá usuário, serei seu assistente para instalação do Java!"
+show_message "Verificando se você possui o Java instalado na sua máquina..."
+
+# Verifica se o Java já está instalado
 java -version
 if [ $? -eq 0 ]; then
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Você já possui o Java instalado na sua máquina!"
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Vamos atualizar os pacotes..."
+    show_message "Você já possui o Java instalado na sua máquina."
+    show_message "Vamos atualizar os pacotes..."
+    read -p "Pressione Enter para continuar..."
     sudo apt update && sudo apt upgrade -y
     clear
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Pacotes atualizados!"
+    show_message "Pacotes atualizados!"
     # Navega até o diretório Desktop
     cd /home/$USER/Desktop
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Diretório Desktop acessado!"
-    sleep 7
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Agora iremos baixar nosso arquivo JAR..."
+    show_message "Diretório Desktop acessado!"
+    show_message "Agora iremos baixar nosso arquivo JAR..."
     # Baixa o arquivo JAR
-   wget https://github.com/SPTECH-Nowl/SistemaJava/Java/main/target/sistema-nowl-1.0-jar-with-dependencies.jar
-   chmod 777  sistema-nowl-1.0-jar-with-dependencies.jar
-    sleep 7
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Já temos o arquivo! Vamos executá-lo."
-    sleep 7
+    wget https://github.com/SPTECH-Nowl/SistemaJava/Java/main/target/sistema-nowl-1.0-jar-with-dependencies.jar
+    chmod 777 sistema-nowl-1.0-jar-with-dependencies.jar
+    show_message "Já temos o arquivo! Vamos executá-lo."
+    show_message "Pressione Enter para iniciar a execução..."
     # Executa o arquivo JAR
     java -jar sistema-nowl-1.0-jar-with-dependencies.jar
 else
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Não foi encontrada nenhuma versão do Java na sua máquina, mas iremos resolver isso!"
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Você deseja instalar o Java na sua máquina (S/N)?"
+    show_message "Não foi encontrada nenhuma versão do Java na sua máquina."
+    show_message "Você deseja instalar o Java na sua máquina (S/N)?"
     read inst
     if [ "$inst" == "S" ]; then
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Ok! Você decidiu instalar o Java na máquina, uhul!"
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Adicionando o repositório!"
+        show_message "Ok! Você decidiu instalar o Java na máquina, uhul!"
+        show_message "Adicionando o repositório..."
         sleep 7
         sudo add-apt-repository ppa:linuxuprising/java
         clear
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Atualizando os pacotes... Quase acabando."
+        show_message "Atualizando os pacotes... Quase acabando."
         sleep 7
         sudo apt update -y
         clear
         if [ $VERSAO -eq 17 ]; then
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
-            sudo apt-get install openjdk-17-jdk
+            show_message "Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
+            sudo apt-get install openjdk-17-jdk -y
             clear
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Java instalado com sucesso!"
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Vamos atualizar os pacotes..."
+            show_message "Java instalado com sucesso!"
+            show_message "Vamos atualizar os pacotes..."
             sudo apt update && sudo apt upgrade -y
             clear
             # Navega até o diretório Desktop
             cd /home/$USER/Desktop
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Diretório Desktop acessado!"
-            sleep 7
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Agora iremos baixar nosso arquivo JAR..."
+            show_message "Diretório Desktop acessado!"
+            show_message "Agora iremos baixar nosso arquivo JAR..."
             # Baixa o arquivo JAR
             wget https://github.com/SPTECH-Nowl/SistemaJava/Java/main/target/sistema-nowl-1.0-jar-with-dependencies.jar
             sleep 7
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Já temos o arquivo! Vamos executá-lo."
-            sleep 7
+            show_message "Já temos o arquivo! Vamos executá-lo."
+            show_message "Pressione Enter para iniciar a execução..."
             # Executa o arquivo JAR
             java -jar sistema-nowl-1.0-jar-with-dependencies.jar
             chmod 777 sistema-nowl-1.0-jar-with-dependencies.jar
         fi
     else
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Você optou por não instalar o Java por enquanto, até a próxima então!"
+        show_message "Você optou por não instalar o Java por enquanto, até a próxima então!"
     fi
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
