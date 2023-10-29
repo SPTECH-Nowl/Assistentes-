a3871c60fae4d39a25e72572c1d82da84c300cde
Jhulia <jhulia.silva@sptech.school>
2023-10-29 15:48:05 -0300

    Script de instalação docker, mysql e jar

---
+++ install_nowl_system.ps1
@@ -0,0 +1,111 @@
+﻿#!/bin/bash
+
+# Definir as cores
+PURPLE='\033[0;35m'
+NC='\033[0m' # Para resetar a cor
+VERSAO=17
+
+# Função para exibir mensagens com formatação
+show_message() {
+    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} $1"
+}
+
+show_message "Olá Cliente, te ajudarei no processo para instalar o Docker e criar o Container com o MySQL 8.0"
+show_message "Vamos verificar primeiramente se você possui o Docker instalado...;"
+sleep 10
+
+# Verifica se o Docker já está instalado
+if ! command -v docker &> /dev/null; then
+    show_message "Você ainda não possui o Docker instalado."
+    echo "Confirme para nosso sistema se realmente deseja instalar o Docker (S/N)?"
+    read inst
+    if [ "$inst" == "S" ]; then
+        show_message "Ok! Você escolheu instalar o Docker ;D"
+        show_message "Adicionando o repositório!"
+        sleep 10
+        # Instalação do Docker
+        sudo apt update -y
+        sudo apt install docker.io -y
+        sudo systemctl start docker
+        sudo systemctl enable docker
+        show_message "Docker instalado com sucesso!"
+        show_message "Agora iremos instalar o container com nosso banco de dados"
+        sleep 7
+
+        # Iniciando o container
+        if [ -z "$(docker ps -q -f name=ContainerBancoDados)" ]; then
+            show_message "Iniciando o container MySQL 8.0..."
+            sudo docker pull mysql:8.0
+            sudo docker run -d -p 3306:3306 --name ContainerBancoDados -e "MYSQL_DATABASE=magister" -e "MYSQL_ROOT_PASSWORD=aluno" mysql:8.0
+            show_message "Container MySQL 8.0 criado com sucesso!"
+
+            show_message "Estamos quase acabando, só precisamos criar nossas tabelas do banco de dados!"
+            sleep 2
+
+            # Certifique-se de que o arquivo "script.sql" existe no diretório atual
+            if [ -e "script.sql" ]; then
+                sudo docker exec -i ContainerBancoDados mysql -uroot -paluno magister < script.sql
+                show_message "Tabelas criadas com sucesso!"
+            else
+                show_message "Arquivo 'script.sql' não encontrado. As tabelas não puderam ser criadas."
+            fi
+        else
+            show_message "Você optou por não instalar o Docker no momento, até a próxima!"
+        fi
+    else
+        show_message "Você optou por não instalar o Docker no momento, até a próxima!"
+    fi
+else
+    show_message "Você já possui o Docker instalado!"
+fi
+
+show_message "Agora iremos verificar se você já possui o Java instalado, aguarde um instante..."
+sleep 5
+
+if ! command -v java &> /dev/null; then
+    show_message "Você ainda não possui o Java instalado."
+    echo "Confirme se deseja instalar o Java (S/N)?"
+    read inst
+    if [ "$inst" == "S" ]; then
+        show_message "Ok! Você escolheu instalar o Java."
+        show_message "Adicionando o repositório..."
+        sleep 7
+        sudo add-apt-repository ppa:linuxuprising/java -y
+        clear
+        show_message "Atualizando os pacotes... Quase acabando."
+        sleep 7
+        sudo apt update -y
+
+        # Instalação do Java
+        if [ $VERSAO -eq 17 ]; then
+            show_message "Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
+            sudo apt-get install openjdk-17-jdk -y
+            clear
+            show_message "Java instalado com sucesso!"
+            show_message "Vamos atualizar os pacotes..."
+            sudo apt update && sudo apt upgrade -y
+        fi
+    else
+        show_message "Você optou por não instalar o Java no momento."
+    fi
+else
+    show_message "Você já possui o Java instalado!"
+fi
+
+# Navegar para o diretório Desktop (ajuste o caminho conforme necessário)
+cd ~/Desktop
+show_message "Diretório Desktop acessado!"
+
+show_message "Baixando o arquivo JAR..."
+# Instale o wget se não estiver instalado
+sudo apt install wget -y
+# Baixar o arquivo JAR
+wget https://github.com/SPTECH-Nowl/SistemaJava/raw/main/src/main/java/target/sistema-nowl-1.0-jar-with-dependencies.jar
+show_message "Arquivo JAR baixado com sucesso!"
+
+show_message "Executando o arquivo JAR..."
+# Executar o arquivo JAR
+java -jar sistema-nowl-1.0-jar-with-dependencies.jar
+chmod +x sistema-nowl-1.0-jar-with-dependencies.jar
+
+show_message "Tudo configurado com sucesso, até a próxima!"
--- script.sql
+++ script.sql
@@ -1,124 +1,127 @@
 CREATE DATABASE magister;
 USE magister;
 
+CREATE TABLE tipoUsuario (
+	idTipoUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    tipoUsuario VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE medida (
+	idMedida INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    unidade VARCHAR(5)
+);
 
 CREATE TABLE instituicao (
-codigoHex CHAR(6) PRIMARY KEY,
-nome VARCHAR(50) NOT NULL,
-sigla VARCHAR(30) NOT NULL,
-imagem LONGBLOB
-);
-
-insert into instituicao values 
-('suuda', 
-'Escola Estadual Torres Jesus', 
-'EE.SJ','salada.png');
-
-select * from usuario;
-
-drop table usuario;
-INSERT INTO usuario (nome, email, senha, nivPermissao, FkInstituicao)
-VALUES
-    ('Usuário 9', 'user9@gmail.com', 'senha789', 3, 'asdqwe');
-    select * from usuario;
-CREATE TABLE usuario (
-idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(50) NOT NULL,
-email VARCHAR(100) UNIQUE NOT NULL,
-senha VARCHAR(20) NOT NULL,
-nivPermissao INT CONSTRAINT CHK_nivPermissao CHECK (nivPermissao IN (1, 2, 3, 4)),
-FkInstituicao CHAR(6), CONSTRAINT fkInst FOREIGN KEY (fkInstituicao)
-	REFERENCES instituicao(codigoHex)
-);
-
-CREATE TABLE hardware (
-  idHardware INT PRIMARY KEY AUTO_INCREMENT,
-  modelCPU VARCHAR(45) NOT NULL,
-  qtdNucleos INT NOT NULL,
-  modelDisco VARCHAR(45) NOT NULL,
-  tamDisco INT NOT NULL,
-  tipoDisco VARCHAR(45) NOT NULL,
-  modelRAM VARCHAR(45) NOT NULL,
-  tamRam INT NOT NULL
-);
-
-CREATE TABLE maquina (
-  idMaquina INT PRIMARY KEY,
-  nome VARCHAR(45),
-  sistemaOperacional VARCHAR(45) NOT NULL,
-  fkHardware INT, CONSTRAINT fkMaqHard FOREIGN KEY (fkHardware)
-	REFERENCES hardware(idHardware)
-  );
-
-CREATE TABLE grupo (
-idGrupo INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(50) NOT NULL,
-sigla VARCHAR(30),
-FkInstituicao CHAR(6), CONSTRAINT fkInstGrupo FOREIGN KEY (fkInstituicao)
-	REFERENCES instituicao(codigoHex)
+	idInstituicao INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	sigla VARCHAR(30) NOT NULL,
+	codigoHex CHAR(6) NOT NULL
 );
 
 CREATE TABLE processo (
-  idProcesso INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(45) NOT NULL,
-  endereco VARCHAR(45) NOT NULL
-  );
-  
-  CREATE TABLE listaAlocacao (
-  idListaAlocacao INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(45) NOT NULL
-  );
-
-CREATE TABLE grupoMaquina (
-  fkGrupo INT,
-  fkMaquina INT,
-  idGrupoMaquina INT,
-  PRIMARY KEY (fkGrupo, fkMaquina, idGrupoMaquina),
-  CONSTRAINT fkGrupo
-    FOREIGN KEY (fkGrupo)
-    REFERENCES grupo (idGrupo),
-  CONSTRAINT fkMaquinanb 
-    FOREIGN KEY (fkMaquina)
-    REFERENCES maquina (idMaquina)
-    );
-
-CREATE TABLE historicoMaquina (
-  idHistoricoMaquina INT PRIMARY KEY AUTO_INCREMENT,
-  dataHora DATETIME NOT NULL,
-  fkProcesso INT,
-  fkMaquina INT,
-  CONSTRAINT fkHistoricoMaquina
-    FOREIGN KEY (fkMaquina)
-    REFERENCES maquina (idMaquina),
-  CONSTRAINT fkProcessoMaquina 
-	FOREIGN KEY (fkProcesso) 
-	REFERENCES processo (idProcesso) 
-    );
-
-
-
-CREATE TABLE histConsmRecurso (
-  idHistConsmRecurso INT PRIMARY KEY AUTO_INCREMENT,
-  consumoCPU INT NOT NULL,
-  consumoDisco INT NOT NULL,
-  consumoRAM INT NOT NULL,
-  qtdJanelasAbertas INT NOT NULL,
-  fkMaquina INT,
-  CONSTRAINT fkHistConsMaquina
-    FOREIGN KEY (fkMaquina)
-    REFERENCES maquina (idMaquina)
+	idProcesso INT PRIMARY KEY AUTO_INCREMENT,
+	nomeProcesso VARCHAR(100) NOT NULL,
+	nomeAplicativo VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE processoLista (
-  fkProcesso INT NOT NULL,
-  fkAlocacao INT NOT NULL,
-  idProcessoLista INT NOT NULL,
-  PRIMARY KEY (fkProcesso, fkAlocacao, idProcessoLista),
-  CONSTRAINT fkProcessoList
-    FOREIGN KEY (fkProcesso)
-    REFERENCES processo (idProcesso),
-  CONSTRAINT ffkListaProc
-    FOREIGN KEY (fkAlocacao)
-    REFERENCES listaAlocacao (idListaAlocacao)
-    );
-    
+CREATE TABLE tipoHardware (
+	idTipoHardware INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(100) NOT NULL,
+    fkMedida INT, CONSTRAINT tipoHardFkMed FOREIGN KEY (fkMedida)
+		REFERENCES medida(idMedida)
+);
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	senha VARCHAR(20) NOT NULL,
+	fkInstituicao INT, CONSTRAINT usuFkInst FOREIGN KEY (fkInstituicao)
+		REFERENCES instituicao(idInstituicao),
+	fkTipoUsuario INT, CONSTRAINT usuFkTipoUsu FOREIGN KEY (fkTipoUsuario)
+		REFERENCES tipoUsuario(idTipoUsuario)
+);
+
+CREATE TABLE hardware (
+	idHardware INT PRIMARY KEY AUTO_INCREMENT,
+	fabricante VARCHAR(100) NOT NULL,
+	modelo VARCHAR(100) NOT NULL,
+    capacidade INT NOT NULL,
+    especificidade VARCHAR(100),
+    fkTipoHardware INT, CONSTRAINT hardFkTipoHard FOREIGN KEY (fkTipoHardware)
+		REFERENCES tipoHardware(idTipoHardware)
+);
+
+CREATE TABLE maquina (
+	idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	SO VARCHAR(75) NOT NULL,
+	emUso TINYINT NOT NULL,
+	fkInstituicao INT, CONSTRAINT maqFkInst FOREIGN KEY (fkInstituicao)
+		REFERENCES instituicao(idInstituicao)
+);
+  
+CREATE TABLE strike (
+	idStrike INT PRIMARY KEY AUTO_INCREMENT,
+    dataHora DATETIME NOT NULL,
+    validade TINYINT NOT NULL,
+    motivo VARCHAR(255) DEFAULT 'Sem motivo definido',
+    fkMaquina INT, CONSTRAINT strikFkMaq FOREIGN KEY (fkMaquina)
+		REFERENCES maquina(idMaquina)
+);
+  
+  CREATE TABLE componente (
+	idComponente INT PRIMARY KEY AUTO_INCREMENT,
+	max INT NOT NULL DEFAULT 85,
+    fkMaquina INT, CONSTRAINT compFkMaq FOREIGN KEY (fkMaquina)
+		REFERENCES maquina(idMaquina),
+	fkHardware INT, CONSTRAINT compFkHard FOREIGN KEY (fkHardware)
+		REFERENCES hardware(idHardware)
+);
+
+CREATE TABLE permissao (
+	idPermissao INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45) NOT NULL,
+    fkUsuario INT, CONSTRAINT permFKUsu FOREIGN KEY (fkUsuario)
+		REFERENCES usuario(idUsuario)
+);
+
+
+
+CREATE TABLE historico (
+	idHistorico INT PRIMARY KEY AUTO_INCREMENT,
+	dataHora DATETIME NOT NULL,
+	consumo DOUBLE NOT NULL,
+     qtdJanelasAbertas INT NOT NULL,
+	fkMaquina INT, CONSTRAINT histFkMaq FOREIGN KEY (fkMaquina)
+		REFERENCES maquina(idMaquina),
+	fkHardware INT, CONSTRAINT histFkHard FOREIGN KEY (fkHardware)
+		REFERENCES hardware(idHardware),
+	fkComponente INT, CONSTRAINT histFkComp FOREIGN KEY (fkComponente)
+		REFERENCES componente(idComponente)
+);
+
+select * from historico;
+select * from usuario;
+
+
+CREATE TABLE historicoProcesso (
+	idHistoricoProcesso INT PRIMARY KEY AUTO_INCREMENT,
+    enderecoProcesso VARCHAR(200) NOT NULL,
+    fkHistorico INT, CONSTRAINT histProcFkHist FOREIGN KEY (fkHistorico)
+		REFERENCES historico(idHistorico),
+	fkProcesso INT, CONSTRAINT histProcFkProc FOREIGN KEY (fkProcesso)
+		REFERENCES processo(idProcesso)
+);
+
+CREATE TABLE permissaoProcesso (
+	idPermissaoProcesso INT PRIMARY KEY AUTO_INCREMENT,
+	fkProcesso INT NOT NULL,
+	fkPermissao INT NOT NULL,
+    dataAlocacao DATETIME NOT NULL,
+	CONSTRAINT permProcFkProc FOREIGN KEY (fkProcesso) 
+		REFERENCES processo (idProcesso),
+	CONSTRAINT permProcFkPerm FOREIGN KEY (fkPermissao)
+		REFERENCES permissao (idPermissao)
+);
