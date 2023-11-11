2706f5297149163d50843936131d790be96c6c54
Will Dantas <126539225+dantaswilljpg@users.noreply.github.com>
2023-11-11 20:28:01 -0300

    Update jar&&docker.sh

--- jar&&docker.sh
+++ jar&&docker.sh
@@ -1,118 +1,84 @@
-#!/bin/bash
-# Define colors for formatting
-PURPLE='\033[0;35m'
-NC='\033[0m'
-VERSAO=17
+#!/bin/sh
 
+sudo apt update && sudo apt upgrade
+# Instalação do Docker
+sudo apt-get update
+sudo apt-get install docker.io -y
 
-# Function to install Docker and MySQL container
-installDockerAndMySQL() {
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Olá Cliente, te ajudarei no processo para instalar o Docker e criar o Container com o MySQL 5.7"
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Vamos verificar primeiramente se você possui o Docker instalado..."
-    sleep 2
-    docker -v
-    if [ $? -eq 0 ]; then
-        echo ": O senhor(a) já tem o Docker instalado!!!"
-    else
-        echo "Não identificamos nenhuma versão do Docker instalado, porém sem problemas, irei resolver isso agora mesmo!"
-        echo "Confirme para nosso sistema se realmente deseja instalar o Docker (S/N)?"
-        read inst
-        if [ "$inst" == "S" ]; then
-            echo "Ok! Você escolheu instalar o Docker ;D"
-            echo "Adicionando o repositório!"
-            sleep 2
-            sudo apt update -y
-            clear
-            echo "Atualizando! Quase lá."
-            sleep 2
-            sudo apt install docker.io -y
-            sudo systemctl start docker
-            sudo systemctl enable docker
-            sleep 2
-            sudo docker pull mysql:5.7
-            sudo docker run -d -p 3306:3306 --name MagisterNowl -e "MYSQL_DATABASE=magister" -e "MYSQL_ROOT_PASSWORD=aluno" mysql:5.7 < script.sql
-            echo "Docker instalado com sucesso e container criado com sucesso!"
-            sleep 2
-            echo "Agora iremos criar as tabelas no banco de dados"
-            sleep 2
-            sudo docker exec -it MagisterNowl bash
-            echo "Tabelas criadas com sucesso!"
-            echo "Tudo configurado com sucesso!"
-        else
-            echo "Você optou por não instalar o Docker por enquanto."
-        fi
-    fi
-}
+# Iniciar o serviço do Docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
+# Verificar se o container MySQL já existe
+if ! docker ps -a --format '{{.Names}}' | grep -q "magister"; then
+    # Criar e executar o container MySQL
+   sudo docker run -d --name magister -e MYSQL_ROOT_PASSWORD=aluno -p 3306:3306 mysql:latest
 
-# Function to install Java and run the Java application
-installJavaAndRunApplication() {
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Olá usuário, serei seu assistente para instalação do Java!"
-    echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Verificando se você possui o Java instalado na sua máquina!"
-    sleep 2
-    # Verifica se o Java está instalado
-    java -version
-    if [ $? -eq 0 ]; then
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Você já possui o Java instalado na sua máquina!"
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Vamos atualizar os pacotes..."
-        sudo apt update && sudo apt upgrade -y
+    # Aguardar alguns segundos para o container ser criado e iniciado
+    sleep 10
+
+    # Copiar o script SQL para dentro do container
+    sudo docker cp /home/ubuntu/Assistentes-app/script.sql magister:/script.sql
+
+    # Executar o script SQL dentro do container
+    sudo docker exec -i magister mysql -u root -paluno < /script.sql
+    echo "Tabelas criadas com sucesso!"
+else
+    echo "O container MySQL já existe. Ignorando a criação de tabelas."
+fi
+
+echo "Agora iremos verificar se você já possui o Java instalado, aguarde um instante..."
+sleep 5
+
+if ! command -v java &> /dev/null; then
+    echo "Você ainda não possui o Java instalado."
+    echo "Confirme se deseja instalar o Java (S/N)?"
+    read inst
+    if [ "$inst" == "S" ]; then
+        echo "Ok! Você escolheu instalar o Java."
+        echo "Adicionando o repositório..."
+        sleep 7
+        sudo add-apt-repository ppa:linuxuprising/java -y
         clear
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Pacotes atualizados!"
-        # Navega até o diretório Desktop
-        cd /home/$USER/Desktop
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Diretório Desktop acessado!"
-        sleep 2
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Agora iremos baixar nosso arquivo JAR..."
-        # Baixa o arquivo JAR
-        git clone https://github.com/SPTECH-Nowl/SistemaWill/target/sistema-will-1.0-jar-with-dependencies.jar
-        sleep 2
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Já temos o arquivo! Vamos executá-lo."
-        sleep 2
-        # Executa o arquivo JAR
-        java -jar sistema-will-1.0-jar-with-dependencies.jar
-    else
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Não foi encontrada nenhuma versão do Java na sua máquina, mas iremos resolver isso!"
-        echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Você deseja instalar o Java na sua máquina (S/N)?"
-        read inst
-        if [ "$inst" == "S" ]; then
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Ok! Você decidiu instalar o Java na máquina, uhul!"
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Adicionando o repositório!"
-            sleep 2
-            sudo add-apt-repository ppa:linuxuprising/java
+        echo "Atualizando os pacotes... Quase acabando."
+        sleep 7
+        sudo apt update -y
+
+        # Instalação do Java
+        if [ $VERSAO -eq 17 ]; then
+            echo "Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
+            sudo apt-get install openjdk-17-jdk -y
             clear
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Atualizando os pacotes... Quase acabando."
-            sleep 2
-            sudo apt update -y
-            clear
-            if [ $VERSAO -eq 17 ]; then
-                echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
-                sudo apt-get install openjdk-17-jdk
-                clear
-                echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Java instalado com sucesso!"
-                echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Vamos atualizar os pacotes..."
-                sudo apt update && sudo apt upgrade -y
-                clear
-                # Navega até o diretório Desktop
-                cd /home/$USER/Desktop
-                echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Diretório Desktop acessado!"
-                sleep 2
-                echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Agora iremos baixar nosso arquivo JAR..."
-                # Baixa o arquivo JAR
-                 git clone https://github.com/SPTECH-Nowl/SistemaWill/target/sistema-will-1.0-jar-with-dependencies.jar
-                sleep 2
-                sleep 2
-                echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Já temos o arquivo! Vamos executá-lo."
-                sleep 2
-                # Executa o arquivo JAR
-                java -jar sistema-will-1.0-jar-with-dependencies.jar
-            fi
-        else
-            echo -e "${PURPLE}[SPTECH-Nowl]:${NC} Você optou por não instalar o Java por enquanto, até a próxima então!"
+            echo "Java instalado com sucesso!"
+            echo "Vamos atualizar os pacotes..."
+            sudo apt update && sudo apt upgrade -y
         fi
+    else
+        echo "Você optou por não instalar o Java no momento."
     fi
-}
+else
+    echo "Você já possui o Java instalado!"
+fi
 
+# Navegar para o diretório Desktop (ajuste o caminho conforme necessário)
+cd ~/Desktop
+echo "Diretório Desktop acessado!"
 
-# Main script
-installDockerAndMySQL
-installJavaAndRunApplication
+# Verificar se o arquivo JAR já existe
+if [ ! -f "sistema-nowl-1.0-jar-with-dependencies.jar" ]; then
+    echo "Baixando o arquivo JAR..."
+    # Instale o wget se não estiver instalado
+    sudo apt install wget -y
+    # Baixar o arquivo JAR
+    wget https://github.com/SPTECH-Nowl/SistemaJava/raw/main/src/main/java/target/sistema-nowl-1.0-jar-with-dependencies.jar
+    echo "Arquivo JAR baixado com sucesso!"
+else
+    echo "O arquivo JAR já existe. Ignorando o download."
+fi
+
+echo "Executando o arquivo JAR..."
+# Executar o arquivo JAR
+java -jar sistema-nowl-1.0-jar-with-dependencies.jar
+chmod +x sistema-nowl-1.0-jar-with-dependencies.jar
+
+echo "Tudo configurado com sucesso, até a próxima!"
