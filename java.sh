4af3b3c7dd48217dad44e18149a01f1e375f617c
Will Dantas <126539225+dantaswilljpg@users.noreply.github.com>
2023-11-13 09:56:34 -0300

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,44 +1,36 @@
-#!/bin/bash
-
-# Mensagem informativa sobre a verificação do Java
-echo "Iniciando o script de configuração do Docker e Java..."
-echo "Verificando se o Java está instalado..."
+echo "Agora iremos verificar se você já possui o Java instalado, aguarde um instante..."
 sleep 5
 
 if ! command -v java &> /dev/null; then
-    echo "Java não encontrado no sistema."
-    echo "Deseja instalar o Java (S/N)?"
+    echo "Você ainda não possui o Java instalado."
+    echo "Confirme se deseja instalar o Java (S/N)?"
     read inst
     if [ "$inst" == "S" ]; then
-        echo "Preparando para instalar o Java..."
+        echo "Ok! Você escolheu instalar o Java."
         echo "Adicionando o repositório..."
         sleep 7
         sudo add-apt-repository ppa:linuxuprising/java -y
         clear
-        echo "Atualizando os pacotes..."
+        echo "Atualizando os pacotes... Quase acabando."
         sleep 7
         sudo apt update -y
 
         # Instalação do Java
-        echo "Escolha a versão do Java que deseja instalar (8/11/17):"
-        read VERSAO
         if [ $VERSAO -eq 17 ]; then
             echo "Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
             sudo apt-get install openjdk-17-jdk -y
             clear
             echo "Java instalado com sucesso!"
-            echo "Atualizando os pacotes..."
+            echo "Vamos atualizar os pacotes..."
             sudo apt update && sudo apt upgrade -y
-        else
-            echo "Versão do Java não reconhecida ou não selecionada. Saindo..."
-            exit 1
         fi
     else
-        echo "Você optou por não instalar o Java neste momento."
+        echo "Você optou por não instalar o Java no momento."
     fi
 else
-    echo "Java já está instalado!"
+    echo "Você já possui o Java instalado!"
 fi
+
 
 # Verificar se o arquivo JAR já existe
 if [ ! -f "sistema-nowl-1.0-jar-with-dependencies.jar" ]; then
@@ -46,13 +38,9 @@ if [ ! -f "sistema-nowl-1.0-jar-with-dependencies.jar" ]; then
     # Instale o wget se não estiver instalado
     sudo apt install wget -y
     # Baixar o arquivo JAR
-    wget https://github.com/SPTECH-Nowl/SistemaJava/raw/main/src/main/java/target/sistema-nowl-1.0-jar-with-dependencies.jar
-    echo "Arquivo JAR baixado com sucesso!"
+    wget https://github.com/SPTECH-Nowl/SistemaTeste1/target/sistema-magister-1.0-jar-with-dependencies.jar
+  java -jar sistema-magister-1.0-jar-with-dependencies.jar
+echo "Arquivo JAR baixado com sucesso!"
 else
-    echo "O arquivo JAR já existe. Nenhuma ação necessária."
+    echo "O arquivo JAR já existe. Ignorando o download."
 fi
-
-# Chamar o script java.sh
-echo "Configuração do Docker e Java concluída. Chamando o script java.sh..."
-./java.sh
-echo "Script java.sh chamado com sucesso!"
