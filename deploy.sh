f83563a71b2a156295f37650ab49c1fd50c96794
Will Dantas <126539225+dantaswilljpg@users.noreply.github.com>
2023-11-26 17:14:39 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,38 +12,38 @@ FG_COLOR="white"
 
 # Função para instalar o Docker
 install_docker() {
-    whiptail --title "Instalação do Docker" --msgbox "Instalando o Docker..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Instalando o Docker..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     sudo apt install docker.io
-    whiptail --title "Instalação do Docker" --msgbox "Docker instalado com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Docker instalado com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
 
-    whiptail --title "Instalação do Docker" --msgbox "Iniciando o serviço do Docker..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Iniciando o serviço do Docker..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     sudo systemctl start docker
     sudo systemctl enable docker
 
-    whiptail --title "Instalação do Docker" --msgbox "Baixando a imagem do MySQL 5.7..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Baixando a imagem do MySQL 5.7..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     sudo docker pull mysql:5.7
-    whiptail --title "Instalação do Docker" --msgbox "Imagem do MySQL 5.7 baixada com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Imagem do MySQL 5.7 baixada com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
 
-    whiptail --title "Instalação do Docker" --msgbox "Criando e executando o container MySQL..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Criando e executando o container MySQL..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     sudo docker run -d -p 3306:3306 --name magister -e "MYSQL_ROOT_PASSWORD=aluno" mysql:5.7
-    whiptail --title "Instalação do Docker" --msgbox "Container MySQL criado e em execução!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Container MySQL criado e em execução!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
 
-    whiptail --title "Instalação do Docker" --msgbox "Executando o script SQL dentro do container MySQL..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Executando o script SQL dentro do container MySQL..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     sleep 15
     sudo docker exec -i magister mysql -u root -paluno < /home/ubuntu/Assistentes-app/script.sql
-    whiptail --title "Instalação do Docker" --msgbox "Script SQL executado com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Script SQL executado com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
 
-    whiptail --title "Instalação do Docker" --msgbox "Dando permissão de execução ao arquivo java.sh..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Dando permissão de execução ao arquivo java.sh..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     chmod +x java.sh
-    whiptail --title "Instalação do Docker" --msgbox "Permissão concedida com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Permissão concedida com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
 
-    whiptail --title "Instalação do Docker" --msgbox "Executando o arquivo java.sh..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Executando o arquivo java.sh..." 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
     ./java.sh
-    whiptail --title "Instalação do Docker" --msgbox "Arquivo java.sh executado com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
+    whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --msgbox "Arquivo java.sh executado com sucesso!" 10 50 --backtitle "Sistema de Monitoramento Magister" --fb --fg
 }
 
 # Menu de opções usando whiptail com cores
-option=$(whiptail --title "Menu" --menu "Escolha uma opção:" 15 50 5 \
+option=$(whiptail --title "Bem-vindo ao Sistema de Monitoramento da Magister" --menu "Escolha uma opção:" 15 50 5 \
     1 "Instalar Docker e configurar MySQL" \
     2 "Sair" \
     3>&1 1>&2 2>&3)
